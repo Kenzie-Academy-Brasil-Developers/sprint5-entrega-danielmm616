9b3a80efa13eda79bcf3186429ec002f24e3942c
daniels <ddan.mm.16@gmail.com>
2022-05-17 16:18:20 -0300

    feat: entities prontas

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,32 @@
+import { Entity, Column, PrimaryColumn } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+@Entity()
+export class User {
+  @PrimaryColumn("uuid")
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  age: number;
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  updatedAt: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
